8a0a56c533e21deade82b1582964a5b08b39f1e3
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-06-10 00:52:30 -0700

    added fastfetch

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,6 +1,10 @@
 fish_add_path ~/.dotfiles/bin
 set -gx EDITOR hx
 
+function fish_greeting
+    fastfetch
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
